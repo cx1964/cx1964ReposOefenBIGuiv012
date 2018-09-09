d3fdc876227d5712470e6f48f97de23b0c1f3ad9
cx1964 <cx1964@gmail.com>
2018-09-09 15:48:29 +0200

    linux shell scripts tbv git

---
+++ 92_save_all_files_to_central_repository_zonder_branching.sh
@@ -0,0 +1,8 @@
+# Filenaam: 92_save_all_files_to_central_repository_zonder_branching.sh
+# Functie: Schrijf alle files weg naar de centrale repository
+
+git remote add origin https://github.com/cx1964/cx1964ReposOefenBIGuiv012.git
+
+# Gebruik username cx1964@gmail.com
+git push -u origin master
+
---
+++ 99_clone_central_repository.sh
@@ -0,0 +1,4 @@
+# Filenaam: 99_clone_central_repository.sh
+# Functie: clone central repository cx1964ReposOefenBIGuiv012 naar local filesystem
+
+git clone https://github.com/cx1964/cx1964ReposOefenBIGuiv012.git
